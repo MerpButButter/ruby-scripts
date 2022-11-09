9f2d26be3d9771e92bba840c6a07383283592b32
marc <marc.levyon@gmail.com>
2022-11-09 16:13:16 +0200

    Substrings

---
+++ substrings.rb
@@ -0,0 +1,9 @@
+def substrings(string, dictionary)
+  filtered_string = string.downcase
+  matches = 
+    dictionary.reduce([]) do |array,value| 
+      array.concat(filtered_string.scan(value.downcase)) unless filtered_string.scan(value.downcase).empty?
+      array
+    end
+  matches.tally
+end
